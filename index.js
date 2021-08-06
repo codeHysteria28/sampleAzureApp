0834349494f6bfbf89605d3a94a76edf17a1674f
codeHysteria28 <graphicglobal67@gmail.com>
2021-08-06 16:23:00 +0100

    our updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 app.get('/',(req,res) => {
-    res.send('Hello Tobias and Anne');
+    res.send('Hello Tobias and Anne this is our first app in NodeJS !!! Hello World.');
 });
 
 app.listen(port, () => {
